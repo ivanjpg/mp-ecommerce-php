b4cc0a6264eb86753dda4f0584d9b1ca5442a758
Ivan Pompa-García <ivan.pompa@gmail.com>
2022-06-28 08:20:01 -0500

    Composer name fixed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ivanjpg/mp-ecommerce-php',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e3aedbfa369010f518359439ddcfb5c7c45a088c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ivanjpg/mp-ecommerce-php' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e3aedbfa369010f518359439ddcfb5c7c45a088c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
